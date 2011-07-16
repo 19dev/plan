d7de113d386c5f5b8667a13c877df50477048c92
gdemir <gdemir@bil.omu.edu.tr>
2011-07-16 13:01:18 +0300

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,17 @@
 class HomeController < ApplicationController
-   def index
-	@head = "ogretim uyesi ders takvimini arama motoru"
-	@departments = Departments.all
+  def index
+    @head = "ogretim uyesi ders takvimini arama motoru"
   end
 
   def find
-	# kontrollere rails'in genel bir önerisi olmalısı lazım'
-	if params[:id].empty?
-		@error = "bolum adi bos birakilamaz"
-		@departments = Departments.all # renderde /home/index'te tekrardan görmüyor
-		return render '/home/index'
-	end
+    # kontrollere rails'in genel bir önerisi olmalısı lazım'
+    if params[:id].empty?
+      @error = "bolum adi bos birakilamaz"
+      @departments = Departments.all # renderde /home/index'te tekrardan görmüyor
+      return render '/home/index'
+    end
 
-	@lecturers = Lecturers.find :all, :conditions => { :department_id => params[:id] }
-	@correct = "hoca bulundu"
+    @lecturers = Lecturers.find :all, :conditions => { :department_id => params[:id] }
+    @correct = "hoca bulundu"
   end
 end
