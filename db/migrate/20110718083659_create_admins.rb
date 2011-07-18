2b273b6ee804fd2bcddb188aaef8148a8db3d156
gdemir <gdemir@bil.omu.edu.tr>
2011-07-18 12:15:22 +0300

    edit superadmin & admin

---
+++ db/migrate/20110718083659_create_admins.rb
@@ -0,0 +1,17 @@
+class CreateAdmins < ActiveRecord::Migration
+  def self.up
+    create_table :admins do |t|
+      t.integer :department_id
+      t.string :first_name
+      t.string :last_name
+      t.string :password
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :admins
+  end
+end
