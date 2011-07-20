eb09f31f4fe46055f33f484a1b06bca56e9b6a1a
gdemir <gdemir@bil.omu.edu.tr>
2011-07-20 17:46:40 +0300

    .

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -11,7 +11,7 @@ class AdminController < ApplicationController
                         "Periods" => 'id',
                         }
     session[:TABLE_INIT] = "Admins"
-    @title = "Yonetici Paneli"
+    session[:error], session[:correct] = nil, nil
     redirect_to '/admin/home' if session[:admin]
   end
 
@@ -21,76 +21,79 @@ class AdminController < ApplicationController
       session[:adminusername] = admin.first_name
       session[:adminpassword] = admin.password
       session[:adminsuper] = true if admin.status == 1
+      session[:error], session[:correct] = nil, nil
 
       table # ilk tablo seçilsin, oyun başlasın!
     else
-      @error = "isim veya sifre hatali"
+      session[:error] = "isim veya sifre hatali"
       return render '/admin/giris'
     end
   end
 
   def logout
-    if session[:admin]
-      reset_session
-#      session[:admin] = nil
-#      session[:adminusername] = nil
-#      session[:adminpassword] = nil
-#      session[:adminsuper] = nil
-#      session[:TABLE_INIT] = nil
-#      session[:TABLES] = nil
-#      session[:TABLE] = nil
-#      session[:KEY] = nil
-    end
+    reset_session if session[:admin]
     redirect_to '/admin/giris'
   end
 
   def table
     table = if params[:table]; params[:table] else session[:TABLE_INIT] end
-    @correct = "#{table} tablosu basariyla secildi"
+    session[:correct] = "#{table} tablosu basariyla secildi"
     session[:TABLE] = table
     session[:SAVE] = eval table.capitalize + ".count"
     session[:KEY] = session[:TABLES][table]
 
-    @title = "Yonetici Panelix"
     render '/admin/home'
   end
 
   def new
-    session[:_key] = nil
+    session[:error], session[:correct], session[:_key] = nil, nil, nil
   end
 
+  # hata var ise oturuma göm; çıkmak isterse nil, doğru ise true dön
   def upload savename, uploaded, overwrite = false
-    destination = Rails.root.join('public', session[:TABLE]) # hedef dizin
+    destination = Rails.root.join 'public', 'images', session[:TABLE] # hedef dizin
     image = destination.join savename # resmin tam yolu
 
     # hedef yoksa oluşturalım
     FileUtils.mkdir(destination) unless File.exist? destination
 
-    message = if uploaded.size > 550000;       "Resim cok buyuk"
-    elsif !uploaded.content_type =~ /jpeg/;    "Resim jpg formatinda olmalidir"
-    elsif File.exist?(image) && !(overwrite);  "Resim zaten var"
-    elsif !FileUtils.mv(uploaded.path, image); "Dosya yukleme hatasi"
-    else nil end
-    return message # mesaj döndüğümüzü belli edelim
+    # yüklenen dosya yok ise sessiz çık
+    return nil unless File.exist? uploaded.path
+
+    if uploaded.size > 550000;                          session[:error] = "Resim cok buyuk"
+    elsif !(uploaded.content_type =~ /jpe?g/);          session[:error] = "Resim jpg formatinda olmalidir"
+    elsif File.exist?(image) && !(overwrite);           session[:error] = "Resim zaten var"
+    elsif !FileUtils.mv(uploaded.path, "#{image}.jpg"); session[:error] = "Dosya yukleme hatasi"
+    else return true end # resim yükleme başarısı
+
+    return nil
   end
 
   def add
-    table = session[:TABLE]
+    session[:error] = nil
 
-    _post = {}
-    eval(table + ".columns").map { |c| _post[c.name] = params[c.name] if c.name != "photo" }
+    photo = params[:file] if params[:file]
+    params.select! { |k, v| eval(session[:TABLE] + ".columns").reduce([]) {|res, c| res << c.name; res}.include?(k) }
 
-    if error = upload(_post[session[:KEY]], params[:file], false) # üzerine yazma olmasın
-      @error = error
-      return render '/admin/new'
-    end
-
-    data = eval table.capitalize + ".new(_post)"
+    data = eval session[:TABLE].capitalize + ".new(params)"
     data.save
     session[:_key] = data[session[:KEY]]
     session[:SAVE] += 1
 
+    # bir resim isteğimiz var mı ?
+    if photo and upload("#{session[:_key]}", photo, false) # üzerine yazma olmasın
+      data[:photo] = "#{session[:TABLE]}/#{session[:_key]}.jpg"
+      data.save
+    else
+      data[:photo] = "default.png"
+      data.save
+    end
+    session[:correct] = "#{session[:_key]} bilgisine sahip kisi #{session[:TABLE]} tablosunu basariyla eklendi"
     show # göster
+  end
+
+  def find
+    session[:error], session[:correct], session[:_key] = nil, nil, nil
   end
 
   def show # post ise oturma göm + verileri göster
@@ -100,6 +103,7 @@ class AdminController < ApplicationController
   end
 
   def review
+    session[:error], session[:correct] = nil, nil
     @data = eval session[:TABLE].capitalize + ".all"
   end
 
@@ -108,22 +112,32 @@ class AdminController < ApplicationController
   end
 
   def del
-    table = session[:TABLE]
-    _key = session[:_key]
+    session[:error], session[:correct] = nil, nil
 
-    eval table + ".delete(_key)"
+    eval session[:TABLE] + ".delete(session[:_key])"
+
+    image = Rails.root.join 'public', 'images', session[:TABLE], "#{session[:_key]}.jpg" # resmimizin tam yolu
+    FileUtils.rm(image) if File.exist? image # resim var ise sil.
     session[:SAVE] -= 1
     session[:_key] = nil # kişinin oturumunu öldürelim
-    @data = eval table.capitalize + ".find :first, :conditions => { session[:KEY] => _key }"
+
     render '/admin/find'
   end
 
   def update
-    table = session[:TABLE]
+    session[:error], session[:correct] = nil, nil
 
-    _post = {}
-    eval(table + ".columns").map { |c| _post[c.name] = params[c.name] }
-    eval table.capitalize + ".update(session[:_key], _post)"
+    photo = params[:file] if params[:file]
+    params.select! { |k, v| eval(session[:TABLE] + ".columns").reduce([]) {|res, c| res << c.name; res}.include?(k) }
+
+    eval session[:TABLE].capitalize + ".update(session[:_key], params)"
+    data = eval session[:TABLE].capitalize + ".find :first, :conditions => { session[:KEY] => session[:_key] }"
+
+    # bir resim isteğimiz var mı ?
+    if photo and upload("#{session[:_key]}", photo, false) # üzerine yazma olmasın
+      data[:photo] = "#{session[:TABLE]}/#{session[:_key]}.jpg"
+      data.save
+    end
 
     show # göster
   end
