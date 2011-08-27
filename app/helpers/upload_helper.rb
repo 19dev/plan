4e62d9d3b527c785e69f18306f04d325d7a69f35
gdemir <gdemir@bil.omu.edu.tr>
2011-08-27 13:52:14 +0300

    . upload

---
+++ app/helpers/upload_helper.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+module UploadHelper
+  # hata var ise oturuma göm; çıkmak isterse nil, doğru ise true dön
+  def upload directory, savename, uploaded, overwrite = false
+    destination = Rails.root.join 'public', 'images', directory # hedef dizin
+    image = destination.join savename # resmin tam yolu
+
+    # hedef yoksa oluşturalım
+    FileUtils.mkdir(destination) unless File.exist? destination
+
+    # yüklenen dosya yok ise sessiz çık
+    return nil unless File.exist? uploaded.path
+
+    if uploaded.size > 550000;                          session[:error] = "Resim çok büyük"
+    elsif !(uploaded.content_type =~ /jpe?g/);          session[:error] = "Resim jpg formatında olmalıdır"
+    elsif File.exist?("#{image}.jpg") && !(overwrite);  session[:error] = "Resim zaten var"
+    elsif !FileUtils.mv(uploaded.path, "#{image}.jpg"); session[:error] = "Dosya yükleme hatası"
+    else return true end # resim yükleme başarısı
+
+    return nil
+  end
+end
