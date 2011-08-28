76c736269dc51934946f54b2d78a810f4665ea4d
gdemir <gdemir@bil.omu.edu.tr>
2011-08-28 12:19:28 +0300

    .

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -2,10 +2,23 @@
 module ScheduleHelper
 # Schedule -------------------------------------------------------
   def schedulenew
-    lecturers = Lecturer.find(:all, :conditions => {:department_id => session[:department_id]})
-    @lecturers = lecturers.select do |lecturer|
-      Assignment.find(:first, :conditions => { :period_id => session[:period_id], :lecturer_id => lecturer.id })
+    lecturers = Lecturer.find(:all, :conditions => { :department_id => session[:department_id] })
+    @assignments = {}
+    lecturers.select do |lecturer|
+      if Assignment.find(:first, :conditions => { :period_id => session[:period_id], :lecturer_id => lecturer.id })
+        c = ""
+        d = Assignment.find(:all, :conditions => { :period_id => session[:period_id], :lecturer_id => lecturer.id })
+        d.each do |ass|
+          if c
+            c = c + ";"
+          end
+          c = c + ass.course_id.to_s+","+ass.course.full_name.to_s
+          #= ass.lecturer.full_name
+        end
+        @assignments[c] = lecturer.full_name
+      end
     end
+
     # courses = Course.find(:all, :conditions => {:department_id => session[:department_id]})
     # @unschedule_courses = courses.select do |course|
     #   !Assignment.find(:first, :conditions => { :course_id => course.id, :period_id => session[:period_id] })
