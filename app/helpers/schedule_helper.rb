b049ead9ea71c8b72ca89c874eaf85cbfeb87ca1
gdemir <gdemir@bil.omu.edu.tr>
2011-08-31 12:30:52 +0300

    .

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -25,6 +25,67 @@ module ScheduleHelper
     # end
   end
   def scheduleadd
+    @assignment = Assignment.find(:first, :conditions => { :period_id => session[:period_id],
+                                             :lecturer_id => params['lecturer_id'],
+                                             :course_id => params['course_id']
+                                              })
+    # @assignment.id
+    schedule = []
+    days = {"Sunday" => "Pazartesi",
+            "Tuesday" => "Salı",
+            "Wednesday" => "Çarşamba",
+            "Thursday" => "Perşembe",
+            "Friday" => "Cuma"}
+
+    # sabah
+    (8..11).each do |hour|
+      days.each do |day_en, day_tr|
+        sec = day_en + hour.to_s + ":15"
+        part = params[sec]
+        if part.length == 2 and part[1] == ""
+            session[:error] = day_tr+hour.to_s+":15"+"  bölümünde sınıf işaretlenmemiş"
+            return redirect_to "/user/schedulenew"
+        elsif part.length == 1 and part[0] != ""
+            session[:error] = day_tr+hour.to_s+":15"+"  bölümünde saat işaretlenmemiş"
+            return redirect_to "/user/schedulenew"
+        elsif part[0] != "" and part[1] != ""
+            schedule << {
+                      'period_id' => session[:period_id],
+                      'assignment_id' => @assignment.id,
+                      'day' => day_en,
+                      'begin_time' => part[0],
+                      'classroom_id' => part[1],
+                    }
+        end
+      end
+    end
+    # akşam
+    (13..22).each do |hour|
+      days.each do |day_en, day_tr|
+        sec = day_en + hour.to_s + ":00"
+        part = params[sec]
+        if part.length == 2 and part[1] == ""
+            session[:error] = day_tr+hour.to_s+":00"+"  bölümünde sınıf işaretlenmemiş"
+            return redirect_to "/user/schedulenew"
+        elsif part.length == 1 and part[0] != ""
+            session[:error] = day_tr+hour.to_s+":00"+"  bölümünde saat işaretlenmemiş"
+            return redirect_to "/user/schedulenew"
+        elsif part[0] != "" and part[1] != ""
+            schedule << {
+                      'period_id' => session[:period_id],
+                      'assignment_id' => @assignment.id,
+                      'day' => day_en,
+                      'begin_time' => part[0],
+                      'classroom_id' => part[1],
+                    }
+        end
+      end
+    end
+    schedule.each do |s|
+      choice = Classplan.new s
+      choice.save
+    end
+
     @a = params
   end
   def scheduleshow
