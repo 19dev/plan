62453b7339489987f43a7ef53a01f9bdb98bfb4c
gdemir <gdemir@bil.omu.edu.tr>
2011-09-01 13:50:03 +0300

    temizlik müdür minor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,10 @@
 # encoding: utf-8
 class HomeController < ApplicationController
-  def index
-    session[:error], session[:notice] = nil, nil
-  end
-
-  def find
-    session[:error], session[:notice] = nil, nil
-  end
+  include CleanHelper # temizlik birimi
+  before_filter :clean_notice # temiz sayfa
+  before_filter :clean_error # temiz sayfa
 
   def review
-    session[:error], session[:notice] = nil, nil
     unless params[:department_id]
       session[:error] = "Bölüm adı boş bırakılamaz"
       return redirect_to '/home/find'
@@ -23,18 +18,16 @@ class HomeController < ApplicationController
   end
 
   def auto
-    session[:error], session[:notice] = nil, nil
     @auto_lecturers = Lecturer.all.collect do |lecturer|
       { lecturer.id => ["#{lecturer.first_name} #{lecturer.last_name}", lecturer.photo, lecturer.department.name] }
     end
   end
 
   def program
-
+    # TODO
   end
 
   def show
-    session[:error], session[:notice] = nil, nil
     unless params[:lecturer_id]
       session[:error] = "Bu isme ait bir öğretim görevlisi bulunamadı"
       return render '/home/auto'
