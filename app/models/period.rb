5df426307ec3797076e93a7dadb05d129635e5ce
gdemir <gdemir@bil.omu.edu.tr>
2011-10-23 09:33:47 +0300

    period güzel gözükmeli

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,5 +1,5 @@
 class Period < ActiveRecord::Base
   def full_name
-    self.year.to_s + '-' + (self.year + 1).to_s + '/' + self.name
+    self.year.to_s + '-' + (self.year + 1).to_s + ' / ' + self.name
   end
 end
