f36806036c1bdad71112ec8c7b63d07d39f832e2
gdemir <gdemir@bil.omu.edu.tr>
2012-02-01 22:39:48 +0200

    pdf: sığmayan dersler için ekle

--- app/helpers/pdf_helper.rb
+++ app/helpers/pdf_helper.rb
@@ -39,7 +39,7 @@ module PdfHelper
         table morning.slice(0..launch_time-1),
           :position => :center,
           :column_widths => { 0=>43.2,1=>73.3,2=>22.7,3=>73.3,4=>22.7,5=>73.3,6=>22.7,7=>73.3,8=>22.7,9=>73.3,10=>22.7},
-          :cell_style => { :size => 5, :text_color => "000000", :height => 35, :border_width => 0.3 }
+          :cell_style => { :size => 5, :text_color => "000000", :height => 35, :border_width => 0.3, :padding => 1 }
 
         table [launch],
           :position => :center,
@@ -50,7 +50,7 @@ module PdfHelper
         table morning.slice(launch_time+1..-1),
           :position => :center,
           :column_widths => { 0=>43.2,1=>73.3,2=>22.7,3=>73.3,4=>22.7,5=>73.3,6=>22.7,7=>73.3,8=>22.7,9=>73.3,10=>22.7},
-          :cell_style => { :size => 5, :text_color => "000000", :height => 35, :border_width => 0.3 }
+          :cell_style => { :size => 5, :text_color => "000000", :height => 35, :border_width => 0.3, :padding => 1}
       end
       if evening
         table evening,
@@ -104,7 +104,7 @@ module PdfHelper
         table morning.slice(0..launch_time-1),
           :position => :center,
           :column_widths => { 0=>43.2,1=>73.3,2=>22.7,3=>73.3,4=>22.7,5=>73.3,6=>22.7,7=>73.3,8=>22.7,9=>73.3,10=>22.7},
-          :cell_style => { :size => 5, :text_color => "000000", :height => 30, :border_width => 0.3 }
+          :cell_style => { :size => 5, :text_color => "000000", :height => 30, :border_width => 0.3, :padding => 1 }
 
         table [launch],
           :position => :center,
@@ -115,13 +115,13 @@ module PdfHelper
         table morning.slice(launch_time+1..-1),
           :position => :center,
           :column_widths => { 0=>43.2,1=>73.3,2=>22.7,3=>73.3,4=>22.7,5=>73.3,6=>22.7,7=>73.3,8=>22.7,9=>73.3,10=>22.7},
-          :cell_style => { :size => 5, :text_color => "000000", :height => 30, :border_width => 0.3 }
+          :cell_style => { :size => 5, :text_color => "000000", :height => 30, :border_width => 0.3, :padding => 1 }
       end
       if evening
         table evening,
           :position => :center,
           :column_widths => { 0=>43.2,1=>73.3,2=>22.7,3=>73.3,4=>22.7,5=>73.3,6=>22.7,7=>73.3,8=>22.7,9=>73.3,10=>22.7},
-          :cell_style => { :size => 5, :text_color => "000000", :height => 25, :border_width => 0.3 }
+          :cell_style => { :size => 5, :text_color => "000000", :height => 25, :border_width => 0.3, :padding => 1 }
       end
     end
     pdf
@@ -174,7 +174,7 @@ module PdfHelper
           table eval("morning#{year}").slice(0..launch_time-1),
             :position => :center,
             :column_widths => { 0=>43.2,1=>73.3,2=>22.7,3=>73.3,4=>22.7,5=>73.3,6=>22.7,7=>73.3,8=>22.7,9=>73.3,10=>22.7},
-            :cell_style => { :size => 5, :text_color => "000000", :height => 35, :border_width => 0.3 }
+            :cell_style => { :size => 5, :text_color => "000000", :height => 35, :border_width => 0.3, :padding => 1 }
 
           table [launch],
             :position => :center,
@@ -185,14 +185,14 @@ module PdfHelper
           table eval("morning#{year}").slice(launch_time+1..-1),
             :position => :center,
             :column_widths => { 0=>43.2,1=>73.3,2=>22.7,3=>73.3,4=>22.7,5=>73.3,6=>22.7,7=>73.3,8=>22.7,9=>73.3,10=>22.7},
-            :cell_style => { :size => 5, :text_color => "000000", :height => 35, :border_width => 0.3 }
+            :cell_style => { :size => 5, :text_color => "000000", :height => 35, :border_width => 0.3, :padding => 1 }
         end
 
         if eval("evening#{year}")
           table eval("evening#{year}"),
             :position => :center,
             :column_widths => { 0=>43.2,1=>73.3,2=>22.7,3=>73.3,4=>22.7,5=>73.3,6=>22.7,7=>73.3,8=>22.7,9=>73.3,10=>22.7},
-            :cell_style => { :size => 5, :text_color => "000000", :height => 30, :border_width => 0.3 }
+            :cell_style => { :size => 5, :text_color => "000000", :height => 30, :border_width => 0.3, :padding => 1 }
         end
         if eval("morning#{year}") and eval("evening#{year}")
           move_down(260)
