d0491b469eb0dd0c074fe7562fee2d648193341d
gdemir <gdemir@bil.omu.edu.tr>
2012-03-16 21:49:22 +0200

    model: ekstra eklemeler, lecturershow: öğretim üyesi dersleri listeleme-1

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ActiveRecord::Base
   has_many :assignment
   belongs_to :department
   has_many :lecturer, :through => :assignment
+  has_many :period, :through => :assignment
   def full_name
     self.code + ' - ' + self.name
   end
--- app/models/period.rb
+++ app/models/period.rb
@@ -1,6 +1,8 @@
 class Period < ActiveRecord::Base
   has_many :assignment
   has_many :classplan
+  has_many :lecturer, :through => :assignment
+  has_many :course, :through => :assignment
   def full_name
     self.year.to_s + '-' + (self.year + 1).to_s + ' / ' + self.name
   end
