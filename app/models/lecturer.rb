56fcc61b0607425b68d42d246292e22549b74b6d
gdemir <gdemir@bil.omu.edu.tr>
2012-03-16 22:24:24 +0200

    lecturershow: lecturer.course(period_id) şeklinde eriş

--- app/models/lecturer.rb
+++ app/models/lecturer.rb
@@ -11,4 +11,12 @@ class Lecturer < ActiveRecord::Base
     assignments.each { |assignment| return true if Classplan.find_all_by_assignment_id_and_period_id(assignment.id, period_id) }
     return false
   end
+  def courses period_id
+    courses = []
+    assignments = Lecturer.find(self.id).assignment.find_all_by_period_id(period_id)
+    assignments.each do |assignment|
+      courses << Course.find(assignment.course_id) if Classplan.find_all_by_assignment_id_and_period_id(assignment.id, period_id)
+    end # Lecturer.find().course -> dersi atamaları gelir ancak hazır olanlar gelmez
+    return courses
+  end
 end
