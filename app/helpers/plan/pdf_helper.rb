8e0a68c3f78c9609b9eaa2689d0e013bc71904e8
gdemir <gdemir@bil.omu.edu.tr>
2012-05-19 12:15:28 +0300

    pdf: margin:1, cell_size:3.6->4

--- app/helpers/plan/pdf_helper.rb
+++ app/helpers/plan/pdf_helper.rb
@@ -2,47 +2,62 @@
 module Plan
   module PdfHelper
     def pdf_schema photo, info, header, field1, field2, meal_time, morning, evening, height
-      pdf = Prawn::Document.new(:page_size => 'A4', :layout => 'portrait') do
+      pdf = Prawn::Document.new(:page_size => 'A4', :layout => 'portrait', :margin => 1) do
           font "#{Prawn::BASEDIR}/data/fonts/comicsans.ttf", :size => 8
 
-          move_up(30)
           text "Ondokuz Mayıs Üniversitesi", :size => 12, :align => :center
           text "Mühendislik Fakültesi Eğitim Öğretim Planları", :size => 8, :align => :center
 
-          image "#{Rails.root}/app/assets/images/omu-logo.jpg", :width => 45, :height => 45, :position => 0, :vposition => -30
-          image "#{Rails.root}/app/assets/images/mf-128x128.png", :width => 45, :height => 45, :position => 475, :vposition => -30
+          image "#{Rails.root}/app/assets/images/omu-logo.jpg", :width => 45, :height => 45, :position => :left, :vposition => 0
+          image "#{Rails.root}/app/assets/images/mf-128x128.png", :width => 45, :height => 45, :position => :right, :vposition => 0
 
           move_up(90)
           if photo
             table info,
-              :position => 221,
-              :column_widths => { 0 => 35, 1 => 90 },
+              :position => 248,
+              :column_widths => { 0 => 35, 1 => 105 },
               :cell_style => { :size => 5, :text_color => "000000", :height => 16, :border_width => 0.1 }
-            image "#{Rails.root}/public#{photo}", :width => 42, :height => 48, :position => 178, :vposition => -8.5
-            move_up(47)
+            image "#{Rails.root}/public#{photo}", :width => 42, :height => 48, :position => 204.5, :vposition => 22
+            move_up(48)
           else
             table info,
-              :position => 166,
-              :column_widths => { 0 => 24, 1 => 160 },
+              :position => :center,
+              :column_widths => { 0 => 24, 1 => 159 },
               :cell_style => { :size => 5, :text_color => "000000", :height => 16, :border_width => 0.1 }
           end
           move_down(1)
 
-          column_widths = {0=>24,1=>53.3,2=>18,3=>53.3,4=>18,5=>53.3,6=>18,7=>53.3,8=>18,9=>53.3,10=>18,11=>53.3,12=>18,13=>53.3,14=>18}
+          column_widths = {
+            0 => 29.5,
+            1 => 62.5,
+            2 => 18,
+            3 => 62.5,
+            4 => 18,
+            5 => 62.5,
+            6 => 18,
+            7 => 62.5,
+            8 => 18,
+            9 => 62.5,
+            10 => 18,
+            11 => 62.5,
+            12 => 18,
+            13 => 62.5,
+            14 => 18
+          }
 
           header_column_widths = { 0 => column_widths[0] }
-          1.upto((column_widths.length - 1) / 2) { |i| header_column_widths[i] = column_widths[1]+column_widths[2] }
+          1.upto((column_widths.length - 1) / 2) { |i| header_column_widths[i] = column_widths[1] + column_widths[2] }
 
           table header,
             :position => :center,
             :row_colors => ["cccccc"],
             :column_widths => header_column_widths,
-            :cell_style => { :size => 3.6, :text_color => "000000", :height => 8, :border_width => 0.1, :padding => 1 }
+            :cell_style => { :size => 4.5, :text_color => "000000", :height => 8, :border_width => 0.1, :padding => 1 }
           table [["", field1, field2, field1, field2, field1, field2, field1, field2, field1, field2, field1, field2, field1, field2],],
             :position => :center,
             :row_colors => ["cccccc"],
             :column_widths => column_widths,
-            :cell_style => { :size => 3.3, :text_color => "000000", :height => 8, :border_width => 0.1, :padding => 1 }
+            :cell_style => { :size => 4, :text_color => "000000", :height => 8, :border_width => 0.1, :padding => 1 }
           if morning
             lunch_time = nil
             morning.each_with_index do |row, index|
@@ -54,31 +69,31 @@ module Plan
             table morning.slice(0..lunch_time-1),
               :position => :center,
               :column_widths => column_widths,
-              :cell_style => { :size => 3.6, :text_color => "000000", :height => height, :border_width => 0.1, :padding => 1 }
+              :cell_style => { :size => 4.5, :text_color => "000000", :height => height, :border_width => 0.1, :padding => 1 }
 
             table morning.slice(lunch_time..lunch_time),
               :position => :center,
               :row_colors => ["cccccc"],
               :column_widths => column_widths,
-              :cell_style => { :size => 3.6, :text_color => "000000", :height => height, :border_width => 0.1, :padding => 1 }
+              :cell_style => { :size => 4.5, :text_color => "000000", :height => height, :border_width => 0.1, :padding => 1 }
 
             table morning.slice(lunch_time+1..-1),
               :position => :center,
               :column_widths => column_widths,
-              :cell_style => { :size => 3.6, :text_color => "000000", :height => height, :border_width => 0.1, :padding => 1 }
+              :cell_style => { :size => 4.5, :text_color => "000000", :height => height, :border_width => 0.1, :padding => 1 }
           end
           if morning and evening
             table [morning[0].collect { |column| "" }], # 2. öğretimi ayıran çizgi
               :position => :center,
               :row_colors => ["cccccc"],
               :column_widths => column_widths,
-              :cell_style => { :size => 3.6, :text_color => "000000", :height => 6, :border_width => 0.1, :padding => 1 }
+              :cell_style => { :size => 4.5, :text_color => "000000", :height => 6, :border_width => 0.1, :padding => 1 }
           end
           if evening
             table evening,
               :position => :center,
               :column_widths => column_widths,
-              :cell_style => { :size => 3.6, :text_color => "000000", :height => height, :border_width => 0.1, :padding => 1 }
+              :cell_style => { :size => 4.5, :text_color => "000000", :height => height, :border_width => 0.1, :padding => 1 }
           end
           move_down(5)
           text "http://plan.mf.omu.edu.tr", :size => 5, :align => :center
